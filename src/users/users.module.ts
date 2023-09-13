8597bd9d19079a2344df10e710d9438e7a38066f
Toxxxy <tonyxxxq@gmail.com>
2023-09-13 19:50:57 +0300

    added controller and service, fixed module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User, UserSchema } from './models/User';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
 export class UsersModule {}
