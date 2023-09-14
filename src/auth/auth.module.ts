c78613ea1b5887493cd7add6de87e8649d47d5a8
Toxxxy <tonyxxxq@gmail.com>
2023-09-14 14:42:33 +0300

    built interface, controller and added service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
-@Module({})
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [
+    forwardRef(() => UsersModule),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET_KEY'),
+        signOptions: {
+          expiresIn: `${configService.get('EXPIRES_IN')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { LoginUserRequestDto } from 'src/users/dto/request/login-user-request.dto';
+import { GetUserResponseDto } from 'src/users/dto/response/get-user-response.dto';
+import { UsersService } from 'src/users/users.service';
+import { TokenPayload } from './interfaces/token-payload';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+
+  async login(
+    loginUserRequest: LoginUserRequestDto,
+  ): Promise<GetUserResponseDto> {
+    const { username, password } = loginUserRequest;
+
+    const user = await this.usersService.validateUser(username, password);
+
+    const accessToken = await this.generateAccessToken(user);
+
+    return {
+      ...user,
+      access_token: accessToken,
+    };
+  }
+
+  async generateAccessToken(user: GetUserResponseDto): Promise<string> {
+    const payload: TokenPayload = {
+      userId: user._id,
+      username: user.username,
+    };
+    return this.jwtService.sign(payload);
+  }
+}
