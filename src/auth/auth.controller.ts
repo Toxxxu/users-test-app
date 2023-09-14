3488fcf155c4ee45857c3d2a786c6792dacfcba3
Toxxxy <tonyxxxq@gmail.com>
2023-09-14 15:33:32 +0300

    added profile check by bearer token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { LoginUserRequestDto } from 'src/users/dto/request/login-user-request.dto';
+import { GetUserResponseDto } from 'src/users/dto/response/get-user-response.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(
+    @Body() loginUserRequest: LoginUserRequestDto,
+  ): Promise<GetUserResponseDto> {
+    return this.authService.login(loginUserRequest);
+  }
+}
